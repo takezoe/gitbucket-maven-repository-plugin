409e2a6ae53d90b4c3e59cf0ccf75def834e2d31
takezoe <takezoe@gmail.com>
2026-09-20 19:41:58 +0900

    Add Version("1.10.0")

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -41,7 +41,8 @@ class Plugin extends gitbucket.core.plugin.Plugin with MavenRepositoryService {
     new Version("1.6.0"),
     new Version("1.7.0"),
     new Version("1.8.0"),
-    new Version("1.9.0")
+    new Version("1.9.0"),
+    new Version("1.10.0")
   )
 
   override val sshCommandProviders = Seq({
